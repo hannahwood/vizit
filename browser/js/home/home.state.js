a38949d634a86254d0d270e8625e02592e9c6c17
Hannah <hannahkowood@gmail.com>
2016-05-16 17:27:35 -0400

    broken code being used for diff

--- browser/js/home/home.state.js
+++ browser/js/home/home.state.js
@@ -1,12 +1,7 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html',
-        resolve: {
-            code: function(){
-                return Promise.resolve('test');
-            }
-        }
+        templateUrl: 'js/home/home.html'
     });
 });
 
