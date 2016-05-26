3a8493e078cc93de010ee70ca4e93f8238249ab2
Jason Unger <jasonscottunger@gmail.com>
2016-05-26 12:43:14 -0400

    More tests done

--- server/utils/auth.middleware.js
+++ server/utils/auth.middleware.js
@@ -16,12 +16,12 @@ Auth.isSelf = function (req) {
    return req.user._id.equals(req.requestedUser._id);
 };
 
-Auth.assert = function (assertion) {
+Auth.assert = function (assertion, status) {
    return function (req, res, next) {
       if (assertion(req)) next();
       else {
          var err = new Error("Not Authenticated");
-         err.status = 401;
+         err.status = status || 401;
          next(err);
       }
    };
@@ -29,12 +29,12 @@ Auth.assert = function (assertion) {
 
 Auth.assertAuthenticated = Auth.assert(Auth.isAuthenticated);
 
-Auth.assertAdmin = Auth.assert(Auth.isAdmin);
+Auth.assertAdmin = Auth.assert(Auth.isAdmin, 403);
 
 Auth.assertSelf = Auth.assert(Auth.isSelf);
 
 Auth.assertAdminOrSelf = Auth.assert(function (req) {
     return Auth.isAuthenticated(req) && (Auth.isAdmin(req) || Auth.isSelf(req));
-});
+}, 403);
 
 module.exports = Auth;
--- tests/server/routes/user-route-test.js
+++ tests/server/routes/user-route-test.js
@@ -30,9 +30,9 @@ describe('Members Route', function () {
          guestAgent = supertest.agent(app);
       });
 
-      it('should get a 401 response if not an admin', function (done) {
+      it('should get a 403 response if not logged in', function (done) {
          guestAgent.get('/api/users/')
-            .expect(401)
+            .expect(403)
             .end(done);
       });
 
@@ -53,13 +53,102 @@ describe('Members Route', function () {
             loggedInAgent.post('/login').send(userInfo).end(done);
          });
 
-         it('should still get a 401 response', function (done) {
+         it('should still get a 403 response accessing all users', function (done) {
             loggedInAgent.get('/api/users/')
-            .expect(401)
+            .expect(403)
             .end(done);
          })
       })
 
    });
+
+   describe('Authenticated login', function () {
+      var loggedInAgent;
+
+      var userInfo = [{
+            email: 'neilpatrickharris@himym.com',
+            password: 'legendary',
+            isAdmin: true
+         }, {
+            email: 'alfred@bat.cave',
+            password: 'whatarecomputers'
+         }]
+
+      beforeEach('Create users', function (done) {
+         User.create(userInfo, done);
+      })
+
+      describe('as admin', function () {
+
+         beforeEach('Create loggedIn user agent and authenticate', function (done) {
+            loggedInAgent = supertest.agent(app);
+            loggedInAgent.post('/login').send(userInfo[0]).end(done);
+         });
+
+         it('can access all users', function (done) {
+            loggedInAgent.get('/api/users/')
+            .expect(200)
+            .end(done);
+         })
+
+      })
+
+      describe('as non-admin', function () {
+
+         beforeEach('Create loggedIn user agent and authenticate', function (done) {
+            loggedInAgent = supertest.agent(app);
+            loggedInAgent.post('/login').send(userInfo[1]).end(done);
+         });
+
+         it('can access user info', function (done) {
+            User.findOne({email: 'alfred@bat.cave'})
+            .then(function (alfred) {
+               loggedInAgent.get(`/api/users/${alfred._id}`)
+               .expect(200)
+               .end(done);
+            })
+            .catch(done);
+         })
+
+         describe('can update', function () {
+
+            it('user info', function (done) {
+               User.findOne({email: 'alfred@bat.cave'})
+               .then(function (alfred) {
+                  expect(alfred.email).to.equal('alfred@bat.cave');
+                  loggedInAgent.put(`/api/users/${alfred._id}`)
+                  .send({email: 'alfred@batcave.wayne'})
+                  .end(function (err, res) {
+                     expect(res.body.email).to.equal('alfred@batcave.wayne');
+                     done();
+                  });
+               })
+               .catch(done);
+            })
+
+            it('user password', function (done) {
+               User.findOne({email: 'alfred@bat.cave'})
+               .then(function (alfred) {
+                  loggedInAgent.put(`/api/users/${alfred._id}`)
+                  .send({oldPassword: 'whatarecomputers', password: 'computer'})
+                  .expect(201)
+                  .end(done);
+               })
+               .catch(done);
+            })
+         })
+
+         it('can delete themself', function (done) {
+            User.findOne({email: 'alfred@bat.cave'})
+            .then(function (alfred) {
+               loggedInAgent.del(`/api/users/${alfred._id}`)
+               .expect(204)
+               .end(done);
+            })
+            .catch(done);
+         })
+
+      })
+   })
 
 });
